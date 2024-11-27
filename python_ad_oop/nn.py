d1d68fab269bb013d58e77d33641f5b049c91bf6
ethanherron <edherron@iastate.edu>
2024-11-27 17:59:20 -0600

    clean

--- python_ad_oop/nn.py
+++ python_ad_oop/nn.py
@@ -1,8 +1,8 @@
-# utils.py
+# nn.py
 
 import numpy as np
 
-from engine import Activation, Operation, Variable
+from engine import Variable, Activation, Operation, Variable
 
 class ReLU(Activation):
     @staticmethod
@@ -127,4 +127,36 @@ class ScheduleFreeAdamW:
             param.zero_grad()
             
             
+class Linear:
+    def __init__(self, in_features, out_features, activation=None):
+        self.W = Variable(np.random.randn(in_features, out_features) * np.sqrt(2. / in_features))
+        self.b = Variable(np.zeros(out_features))
+        self.activation = activation
+        
+    def __call__(self, x):
+        linear_output = (x @ self.W) + self.b
+        if self.activation:
+            return self.activation.apply(linear_output)
+        return linear_output
+
+    def parameters(self):
+        return self.W, self.b
+    
+class MLP:
+    def __init__(self, in_features, out_features, hidden_features, num_layers):
+        self.mlp = [Linear(in_features, hidden_features, ReLU)]
+        for _ in range(num_layers):
+            self.mlp.append(Linear(hidden_features, hidden_features, ReLU))
+        self.mlp.append(Linear(hidden_features, out_features))
+        
+    def __call__(self, x):
+        for layer in self.mlp:
+            x = layer(x)
+        return x
+    
+    def parameters(self):
+        params = []
+        for layer in self.mlp:
+            params.extend(layer.parameters())
+        return params
             
